ed7d96049c805ade9fc1bf4093f4ecc69f592a6c
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-11-11 17:06:13 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,16 +3,14 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log("TEST");
-console.log(galleryItems);
 
+console.log(galleryItems);
 const $gallery = document.querySelector('div.gallery');
 
 galleryItems.forEach(element => {
   const $galleryLink = document.createElement('a');
   $galleryLink.classList.add('gallery__item');
-    $galleryLink.setAttribute('href', element.original);
-    console.log("test");
+  $galleryLink.setAttribute('href', element.original);
 
   const $galleryImage = document.createElement('img');
   $galleryImage.classList.add('gallery__image');
