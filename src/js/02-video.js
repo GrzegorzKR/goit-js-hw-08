8f49d3362ef7fec58ceef55f6e8abc7632b16b5b
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-11-11 21:46:27 +0100

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const savedPlayerTime = ({ duration, percent, seconds }) => {
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(savedPlayerTime, 1000));
+
+let lastTime = localStorage.getItem('videoplayer-current-time');
+player
+
+  .setCurrentTime(lastTime)
+  .play()
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
