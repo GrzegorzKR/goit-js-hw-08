a44f9eff2dcd54fb3f759dde8970f20575db5b01
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-11-12 20:11:04 +0100

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form.feedback-form');
+form.addEventListener('input', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log(`email:  ${email.value}, message  ${message.value}`);
+}
+console.log('test');
