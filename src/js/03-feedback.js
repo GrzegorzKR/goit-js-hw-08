0af3bd832dd4dd3c6a541727aae750d3340cf466
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-11-17 23:10:21 +0100

    Update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('form.feedback-form');
-const emailField = document.querySelector('input');
-const messageField = document.querySelector('textarea');
+// const emailField = document.querySelector('input');
+// const messageField = document.querySelector('textarea');
 
 const inputCatcher = event => {
   const {
@@ -18,28 +18,34 @@ const inputCatcher = event => {
   );
 };
 
-form.addEventListener('input', throttle(inputCatcher, 500));
-
-const inputValue = localStorage.getItem('feedback-form-state');
-const parsedInputValue = JSON.parse(inputValue);
-
-if (parsedInputValue) {
-  if (parsedInputValue !== '') {
-    emailField.value = parsedInputValue.email;
-  }
-}
-
-if (parsedInputValue) {
-  if (parsedInputValue !== '') {
-    messageField.value = parsedInputValue.message;
-  }
-}
-
 const submitHandler = event => {
   event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (email.value === '' || message.value === '') {
+    alert('Proszę wypełnić wszystkie pola formularza');
+    return;
+  }
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   form.reset();
   localStorage.removeItem('feedback-form-state');
 };
 
+form.addEventListener('input', throttle(inputCatcher, 500));
 form.addEventListener('submit', submitHandler);
+
+// const inputValue = localStorage.getItem('feedback-form-state');
+// const parsedInputValue = JSON.parse(inputValue);
+
+// if (parsedInputValue) {
+//   if (parsedInputValue !== '') {
+//     emailField.value = parsedInputValue.email;
+//   } else console.log('Error email');
+// }
+
+// if (parsedInputValue) {
+//   if (parsedInputValue !== '') {
+//     messageField.value = parsedInputValue.message;
+//   }
+// }
